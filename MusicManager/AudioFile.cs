caf6ec8560f878f40892ec456cac053eb77666cd
aiden lundeen <aiden.lundeen@student.centralia.edu>
2021-06-02 08:02:22 -0700

    simplified buttonFolder code, created class AudioFile

---
+++ MusicManager/AudioFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicManager
+{
+    public class AudioFile
+    {
+        public AudioFile(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        // Meta-Data
+        public string Genre { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        
+    }
+
+
+
+}
--- MusicManager/Form1.cs
+++ MusicManager/Form1.cs
@@ -13,7 +13,7 @@ namespace MusicManager
 {
     public partial class FormMain : Form
     {
-        List<string> songsInFolder = new List<string>(); // Can be made into a list of a song class, which would have more info, or we can just store reference here as is done already
+        List<AudioFile> songsInFolder = new List<AudioFile>(); // Can be made into a list of a song class, which would have more info, or we can just store reference here as is done already
         
         public FormMain()
         {
@@ -47,10 +47,10 @@ namespace MusicManager
             // future potentially required code.
             // Clearing the previous list
             listBoxSelectedFile.Items.Clear();
-            int splitter;
-            string folderpath;
-            folderSelectDialogue = new FolderBrowserDialog();
-            DialogResult dr = folderSelectDialogue.ShowDialog();
+
+        string folderpath;
+        folderSelectDialogue = new FolderBrowserDialog();
+        DialogResult dr = folderSelectDialogue.ShowDialog();
             if (dr == DialogResult.OK)
             {
                 folderpath = folderSelectDialogue.SelectedPath;
@@ -59,23 +59,28 @@ namespace MusicManager
                 string[] files = Directory.GetFiles(folderpath);
                 foreach(string file in files)
                 {
-                    string format = file;
-                    // filter only mp3's
-                    splitter = format.LastIndexOf(".");
-                    format = format.Substring(splitter + 1);
+        // filter only mp3's
+                    string song = file.Substring(file.Length-3,3);
 
-                    if (format.Contains("mp3"))
+                    if (song.Contains("mp3"))
                     {
                         // adding reference to location of the song
-                        songsInFolder.Add(file);
-                        splitter = file.LastIndexOf("\\");
+                        AudioFile mp3file =
+                            new AudioFile
+                            (
+                                file.Substring(
+                                file.LastIndexOf("\\") + 1,
+                                file.LastIndexOf(".") - (file.LastIndexOf("\\") + 1)
+                                )
+                            );
+                        songsInFolder.Add(mp3file);
                         // add visual string of the song filename, splitter+1 removes file path, whereas after the comma removes .mp3
-                        listBoxSelectedFile.Items.Add(file.Substring(splitter + 1, file.LastIndexOf(".") - (splitter + 1)));
+                        listBoxSelectedFile.Items.Add(mp3file.Name);
 
 
                         // Potential improvement, listBox has multicollumn support. Thus, song, album, and artist can be split respectively
                     }
-                }
+}
             }
             
 
